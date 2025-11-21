00155754dac601c427f0b583312c52303aab9798
yarl1k <yaroslawdyr@gmail.com>
2025-11-21 18:45:57 +0200

    added imitations of slow/quich op's and completed work func, according to v_10

---
+++ main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+#include <thread>
+#include <chrono>
+#include <future>
+#include <syncstream>
+#include <iomanip>
+
+using namespace std;
+
+void task(const string& name, int duration) {
+    this_thread::sleep_for(chrono::seconds(duration));
+    osyncstream(cout) << name << endl;
+}
+
+void slow(const string& name) {
+    task(name, 7);
+}
+
+void quick(const string& name) {
+    task(name, 1);
+}
+
+void work() {
+
+    future<void> f_A1 = async(launch::async, slow, "A1");
+    future<void> f_A2 = async(launch::async, slow, "A2");
+
+    slow("D");
+
+    f_A1.wait();
+    f_A2.wait();
+
+    future<void> f_B = async(launch::async, quick, "B");
+
+    quick("F");
+
+    f_B.wait();
+
+    future<void> f_C1 = async(launch::async, quick, "C1");
+
+    quick("C2");
+
+    f_C1.wait();
+
+    osyncstream(cout) << "Work is done!" << endl;
+}
+
+int main() {
+    auto start_time = chrono::steady_clock::now();
+    work();
+
+    auto end_time = chrono::steady_clock::now();
+    chrono::duration<double> work_time = end_time - start_time;
+
+    osyncstream(cout) << "\nЧас виконання: " << fixed << setprecision(2) << work_time.count() << " секунд" << endl;
+    return 0;
+}
